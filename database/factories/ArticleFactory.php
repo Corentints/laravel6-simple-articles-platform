c1f39253546b38c0f61f433b1ce0a100ad99b5ea
THOMAS-Corentin <corentin.tho@gmail.com>
2020-02-25 00:13:44 +0100

    feat: set article factory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -6,7 +6,12 @@ use App\Article;
 use Faker\Generator as Faker;
 
 $factory->define(Article::class, function (Faker $faker) {
+    $title = $this->faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => Str::slug($title),
+        'summary' => $this->faker->realText(200),
+        'content' => $this->faker->text,
+        'author_id' => factory(\App\User::class, ['is_admin' => 1])
     ];
 });
