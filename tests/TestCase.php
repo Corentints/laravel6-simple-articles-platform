9e0227340d5d405868f66da1a20b2fde5479ef1a
THOMAS-Corentin <corentin.tho@gmail.com>
2020-02-25 00:13:13 +0100

    test: actingAsAdmin function

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function actingAsAdmin() {
+        $user = factory('App\User')->create(['is_admin' => true]);
+        $this->actingAs($user);
+        return $user;
+    }
 }
