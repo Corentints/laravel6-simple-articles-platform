62fc2203ffedfcb03d6c079fb9713e16aca22ead
Corentin <corentin.tho@gmail.com>
2020-03-06 02:03:59 +0100

    feat: improve index view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,10 +3,12 @@
     <h1>Admin dashboard - manage the blog</h1>
 
     <section class="articles">
+        <a href="/admin/articles">Manage articles</a>
         Latest articles
     </section>
 
     <section class="categories">
+        <a href="/admin/categories">Manage categories</a>
         Latest categories
     </section>
 
